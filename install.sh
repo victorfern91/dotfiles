513657c9e707fe0fd398fc50fdcf8aca29e7a2fe
Victor Fernandes <victorfern91@gmail.com>
2021-01-31 21:03:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo "hello world"
 # copy bash files to user root dir
-cp .bash_* ~
+#cp .bash_* ~
 
 # copy git configurations
-cp .gitconfig ~
+#cp .gitconfig ~
 
-source ~/.bash_profile
+#source ~/.bash_profile
 
